c2b76b47e719e84d525e12854489baa1e7e836f5
Ladaisha Pettus <ladaishapettus@Administrators-MacBook-Pro.local>
2019-11-25 15:14:45 -0500

    adding audit

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles.css';
+
+const Home = (props) => {
+	return (
+		<>
+			<img src="../HomeImages/slideshow2.jpg"></img>
+
+			<div class="about">
+				<h2>About Us</h2>
+			</div>
+
+			<p>
+				DecorandMore was founded in 2015 by La'Daisha Pettus. She's always had a
+				passion for interior design. Through technology and innovation, Decor&More
+				makes it possible for shoppers to quickly and easily find exactly what
+				they want from a selection of more than 14 million items across home
+				furnishings and décor. Committed to delighting its customers every step of
+				the way, DecorandMore is reinventing the way people shop for their homes -
+				from product discovery to final delivery.
+  </p>
+		</>
+	)
+}
+
+export default Home
